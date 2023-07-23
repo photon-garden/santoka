5ac8c35a2f248f32358d9ea4514d81e839677054
Luca Bailey <mail@photon.garden>
2023-07-23 11:56:16 -0400

    Strip dev-only browser code from production builds.

--- rust/browser/src/dev/mod.rs
+++ rust/browser/src/dev/mod.rs
@@ -1,10 +1,13 @@
-pub fn set_panic_hook() {
+mod automatic_reloading;
+
+pub fn main() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
     // we will get better error messages if our code ever panics.
     //
     // For more details see
     // https://github.com/rustwasm/console_error_panic_hook#readme
-    #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
+
+    automatic_reloading::start_checking_for_updates();
 }
